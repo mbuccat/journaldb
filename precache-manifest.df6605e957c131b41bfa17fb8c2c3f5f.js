846ae997734c6a091deff144cc14e6701cf705e3
mbuccat <mbuccat@bu.edu>
2020-07-16 15:06:14 -0400

    Updates

---
+++ precache-manifest.df6605e957c131b41bfa17fb8c2c3f5f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05216b8f0340699dd6fa5e00a4f3741a",
+    "url": "/journaldb/index.html"
+  },
+  {
+    "revision": "c7aaa06de701a6540d1f",
+    "url": "/journaldb/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "854ec37d25d6b702f4ed",
+    "url": "/journaldb/static/css/main.9bffd347.chunk.css"
+  },
+  {
+    "revision": "c7aaa06de701a6540d1f",
+    "url": "/journaldb/static/js/2.15fb10b9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/journaldb/static/js/2.15fb10b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "854ec37d25d6b702f4ed",
+    "url": "/journaldb/static/js/main.36968542.chunk.js"
+  },
+  {
+    "revision": "e1fc8c974869460c3853",
+    "url": "/journaldb/static/js/runtime-main.6cf63d31.js"
+  },
+  {
+    "revision": "bd3f8b819d9e93ca9fda18991d87f1ce",
+    "url": "/journaldb/static/media/person.bd3f8b81.svg"
+  }
+]);
